5a96be4b4523ff23d17ad6b9bf4d1c95c3ae9076
mohdazamkhanjamia <me.azamkhan@gmail.com>
2022-02-01 18:16:15 +0530

    fail test

--- TestStringMethods.py
+++ TestStringMethods.py
@@ -7,7 +7,7 @@ class TestStringMethods(unittest.TestCase):
 
     # Returns True if the string contains 4 a.
     def test_strings_a(self):
-        self.assertEqual( 'a'*4, 'aaaa')
+        self.assertEqual( 'a'*4, 'aaaaa')
 
     # Returns True if the string is in upper case.
     def test_upper(self):
